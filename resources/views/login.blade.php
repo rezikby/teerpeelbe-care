6e795d7fc976966c18681e1557e6bc2ed2eb24c5
unknown <ppatih99@gmail.com>
2026-03-09 07:04:13 -0700

    update halaman home

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,15 +2,58 @@
 <html>
 <head>
     <title>Login</title>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
 </head>
 <body>
-    <h2>Login</h2>
+  
 
     @if(session('error'))
         <p style="color:red;">{{ session('error') }}</p>
     @endif
+    
+    <div class="container">
+   
 
+    <div class="main">
+      
+        <h1>
+             <i class="fas fa-clinic-medical"></i>
+        Terpelebe <span>Care</span>
+    
+   
+    </h1>
+
+        <div class="kes">
+            <p>KESEHATAN DIGITAL #1 INDONESIA</p>
+        </div>
+
+        <div class="judul">
+            <h1>Satu Platform
+Untuk Semua
+Kebutuhan Sehat</h1>
+
+                  <div class="texs">
+                <p>Bergabung dan dapatkan akses ke dokter terbaik, diagnosis AI canggih, komunitas kesehatan, dan layanan inklusif untuk semua.</p>
+            </div>
+        </div>
+    </div>
+
+<div class="form-wrapper">
     <form method="POST" action="/login">
+       <div class="kata">
+                <a href="{{ route('register') }}" 
+                   class="{{ request()->routeIs('register') ? 'aktif' : '' }}">
+                   Register
+                </a>
+
+                <a href="{{ route('login') }}" 
+                   class="{{ request()->routeIs('login') ? 'aktif' : '' }}">
+                   Login
+                </a>
+            </div>
+
+
         @csrf
         <div>
             <label>Username:</label>
@@ -21,8 +64,10 @@
             <input type="password" name="Password" required>
         </div>
         <button type="submit">Login</button>
+         <p>Belum punya akun? <a href="{{ route('register') }}">Daftar di sini</a></p>
     </form>
+</div>
 
-    <p>Belum punya akun? <a href="{{ route('register') }}">Daftar di sini</a></p>
+   
 </body>
 </html>
